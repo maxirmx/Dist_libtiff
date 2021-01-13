85efd46b4039e6ced015d0dc5f3136c8b690a815
maxirmx <maxim@samsonov.net>
2021-01-13 15:18:58 +0300

    Local

---
+++ local/build.cmd
@@ -0,0 +1,20 @@
+@echo off
+rem --------------------------------------------------------------------
+rem A script to build libtiff locally
+rem Usage build.cmd <platform> <configuration>
+rem        platform: either 'X64' or 'x86'
+rem                  'amd64' is accepted as well and converted to 'X64'
+rem                   (load-library) default is 'X64'
+rem        configuration: either 'debug' or 'release'
+rem                   (load-library) default is 'debug'
+rem --------------------------------------------------------------------
+@echo on
+
+cd ..
+
+IF NOT EXIST libjpeg  (powershell -ExecutionPolicy Bypass %~dp0..\load-libtiff.ps1) 
+powershell -ExecutionPolicy Bypass .\build-libtiff.ps1 %1 %2 -configure
+
+cd %~dp0
+
+
